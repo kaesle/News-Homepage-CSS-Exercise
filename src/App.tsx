09c2dab8ebb2f95aa0274cac2c5218e2aad20c14
kaesle <kaesle@protonmail.com>
2023-03-21 15:02:31 -0500

    static header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import logo from './assets/images/logo.svg';
+import menuOpen from './assets/images/icon-menu.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        header
+        <img id="logo" src={logo} alt="W logo" />
+        <img id="menu-open" src={menuOpen} alt="open menu" />
       </header>
       <main>
         main
