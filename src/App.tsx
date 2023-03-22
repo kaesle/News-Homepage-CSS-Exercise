6c81d4f4b09802c591a717e93479f34042dd39b9
kaesle <kaesle@protonmail.com>
2023-03-21 22:25:43 -0500

    finish mobile site

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,42 @@ import { useState } from 'react';
 
 import MobileMenu from './components/MobileMenu';
 import NewSection from './components/NewSection';
+import ThumbnailStory from './components/ThumbnailStory';
 
 import logo from './assets/images/logo.svg';
 import menuOpenIcon from './assets/images/icon-menu.svg';
 import mobileHero from './assets/images/image-web-3-mobile.jpg';
+import thumb1 from './assets/images/image-retro-pcs.jpg';
+import thumb2 from './assets/images/image-top-laptops.jpg';
+import thumb3 from './assets/images/image-gaming-growth.jpg';
+
+interface Story {
+  imgSrc: string;
+  number: number;
+  headline: string;
+  blurb: string;
+}
+
+const stories: Story[] = [
+  {
+    imgSrc: thumb1,
+    number: 1,
+    headline: "Reviving Retro PCs",
+    blurb: "What happens when old PCs are given modern upgrades?"
+  },
+  {
+    imgSrc: thumb2,
+    number: 2,
+    headline: "Top Ten Laptops of 2022",
+    blurb: "Our best picks for various needs and budgets."
+  },
+  {
+    imgSrc: thumb3,
+    number: 3,
+    headline: "The Growth of Gaming",
+    blurb: "How the pandemic has sparked fresh opportunities."
+  }
+];
 
 export default function App() {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -33,6 +65,15 @@ export default function App() {
           <pre>R E A D  M O R E</pre>
         </button>
         <NewSection />
+        {
+          stories.map(s=>
+            <ThumbnailStory
+              imgSrc={s.imgSrc}
+              number={s.number}
+              headline={s.headline}
+              blurb={s.blurb}
+            />)
+        }
       </main>
     </div>
   );
---
+++ src/components/ThumbnailStory.tsx
@@ -0,0 +1,19 @@
+import './ThumbnailStory.css';
+
+type TNSProps = {
+  imgSrc: string,
+  number: number,
+  headline: string,
+  blurb: string
+};
+
+export default function ThumbnailStory({imgSrc, number, headline, blurb}: TNSProps) {
+  return (
+    <div className="story">
+      <img src={imgSrc} alt={headline} aria-hidden />
+      <h2 className="number">0{number}</h2>
+      <h3 className="headline">{headline}</h3>
+      <p className="blurb">{blurb}</p>
+    </div>
+  );
+};
